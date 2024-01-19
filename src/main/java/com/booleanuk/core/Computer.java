ff8e00cee624905d8a03b3a09651e68204b28742
hanna <hannaphamm1007@gmail.com>
2024-01-19 10:10:40 +0100

    implement exercise

--- src/main/java/com/booleanuk/core/Computer.java
+++ src/main/java/com/booleanuk/core/Computer.java
@@ -3,25 +3,38 @@ package com.booleanuk.core;
 import java.util.ArrayList;
 
 public class Computer {
-    public ArrayList<Game> installedGames = new ArrayList<>();
+    // Comment from class
+    // Trying to avoid create a new or initiate a new ArrayList at top of the class (ex: line 9)
+    // Prefer to have a new ArrayList in the constructor
+    public ArrayList<Game> installedGames;
+    PowerSupply powerSupply;
 
+    public Computer(PowerSupply powerSupply){
+        this.powerSupply = powerSupply;
+        this.installedGames = new ArrayList<>();
+    }
+    public Computer(PowerSupply powerSupply, ArrayList<Game> preInstalled){
+        this.powerSupply = powerSupply;
+        this.installedGames = new ArrayList<>();
+        // Add all the collection to the installedGames
+        this.installedGames.addAll(preInstalled);
+    }
     public void turnOn() {
-        PowerSupply psu = new PowerSupply();
-        psu.turnOn();
+        this.powerSupply.turnOn();
     }
 
-    public void installGame() {
-        Game game = new Game("Morrowind");
+    public void installGame(String nameGame) {
+        Game game = new Game(nameGame);
         this.installedGames.add(game);
-    }
 
-    public String playGame() {
-        for (Game g : this.installedGames) {
-            if (g.name.equals("Morrowind")) {
-                return g.start();
+    }
+    public String playGame(String nameGame) {
+        for(Game game : this.installedGames) {
+            if (game.name.equals(nameGame)) {
+                return game.start();
             }
         }
-
         return "Game not installed";
     }
-}
+    }
+
